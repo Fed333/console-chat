8904fb533e1132a85fddc6598418d9d2616c3c94
Roman <kovalchuk.roman03@gmail.com>
2023-12-31 16:49:18 +0200

    Initial commit

---
+++ app/src/main/java/com/vntu/console/chat/app/ConsoleChatApplication.java
@@ -0,0 +1,11 @@
+package com.vntu.console.chat.app;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConsoleChatApplication {
+
+    public static void main(String[] args) {
+        log.info("Start...");
+    }
+}
